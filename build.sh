c6e63ed6455ff7015510920afaf0a5a3e3c62377
Warpcode <git@warpcode.co.uk>
2020-08-24 19:55:40 +0100

    Remove s6 specific release. Just bundle it as default

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-BUILD_S6=false
 PUSH_TO_DOCKER=false
 DISABLE_BUILDER=false
 while [[ $# -gt 0 ]]
@@ -19,10 +18,6 @@ do
                     ;;
             esac
             shift
-            shift
-            ;;
-        -s6|--s6)
-            BUILD_S6=true
             shift
             ;;
         -i|--image)
@@ -81,56 +76,11 @@ BASE_TAG=
 DEST_IMAGE=
 DEST_TAG=
 
-# Clear s6 vars
-ARCHITECTURES_S6=
-DOCKERFILE_S6=
-BASE_IMAGE_S6=
-BASE_TAG_S6=
-DEST_IMAGE_S6=
-DEST_TAG_S6=
-
 source "versions/$IMAGE_VERSION"
 
 if [[ "$DOCKERFILE" == "" ]]; then
     echo "Skipping $i. No dockerfile supplied"
     continue;
-fi
-
-if [[ $BUILD_S6 == true ]]; then
-    if [[ "$ARCHITECTURES_S6" != "" ]]; then
-        ARCHITECTURES=$ARCHITECTURES_S6
-    fi
-
-    if [[ "$BASE_IMAGE_S6" != "" ]]; then
-        BASE_IMAGE=$BASE_IMAGE_S6
-    else
-        # If no s6 base image is specified, assume we're using a compiled version
-        # we pushed
-        BASE_IMAGE="warpcode/${BASE_IMAGE#*/}-s6"
-    fi
-
-    if [[ "$BASE_TAG_S6" != "" ]]; then
-        BASE_TAG=$BASE_TAG_S6
-    fi
-
-    if [[ "$BASE_IMAGE" == "" ]] || [[ "$BASE_TAG" == "" ]]; then
-        echo "Skipping $i. No S6 base image or tag supplied"
-        continue;
-    fi
-
-    if [[ "$DOCKERFILE_S6" != "" ]]; then
-        DOCKERFILE=$DOCKERFILE_S6
-    else
-        DOCKERFILE="${DOCKERFILE}-s6"
-    fi
-
-    if [[ "$DEST_IMAGE_S6" != "" ]]; then
-        DEST_IMAGE=$DEST_IMAGE_S6
-    fi
-
-    if [[ "$DEST_TAG_S6" != "" ]]; then
-        DEST_TAG=$DEST_TAG_S6
-    fi
 fi
 
 if [[ "$BASE_IMAGE" == "" ]] || [[ "$BASE_TAG" == "" ]]; then
