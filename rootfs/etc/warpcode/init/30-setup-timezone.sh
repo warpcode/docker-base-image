f77238c062150d8de89da641ff0eec8292e2a492
Warpcode <git@warpcode.co.uk>
2021-01-18 21:39:52 +0000

    Better debug info when changing the timezone

--- rootfs/etc/warpcode/init/30-setup-timezone.sh
+++ rootfs/etc/warpcode/init/30-setup-timezone.sh
@@ -2,9 +2,9 @@
 
 if [ -n "${TZ:-}" ]; then
     if [ "${TZ}" != "$(cat /etc/timezone)" ] && [ -f "/usr/share/zoneinfo/\${TZ}" ]; then
+        output_log "Setting Timezone to ${TZ} from {$(cat /etc/timezone)}"
         rm -f /etc/localtime
         cp "/usr/share/zoneinfo/${TZ}" /etc/localtime
         echo "${TZ}" > /etc/timezone
-        output_log "Setting Timezone to ${TZ}"
     fi
 fi
