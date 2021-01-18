88b1534d4fe0d3e597dc224eac78f5866690994f
Warpcode <git@warpcode.co.uk>
2021-01-18 21:47:15 +0000

    Another timezone fix

--- rootfs/etc/warpcode/init/30-setup-timezone.sh
+++ rootfs/etc/warpcode/init/30-setup-timezone.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 if [ -n "${TZ:-}" ]; then
-    if [ "${TZ}" != "$(cat /etc/timezone)" ] && [ -f "/usr/share/zoneinfo/\${TZ}" ]; then
-        output_log "Setting Timezone to ${TZ} from {$(cat /etc/timezone)}"
+    if [ "${TZ}" != "$(cat /etc/timezone)" ] && [ -f "/usr/share/zoneinfo/${TZ}" ]; then
+        output_log "Setting Timezone to ${TZ} from $(cat /etc/timezone)"
         rm -f /etc/localtime
         cp "/usr/share/zoneinfo/${TZ}" /etc/localtime
         echo "${TZ}" > /etc/timezone
