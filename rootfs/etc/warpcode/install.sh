0d730e6d5e450317e25763ee567ee83bc5579439
Warpcode <git@warpcode.co.uk>
2021-01-22 19:36:55 +0000

    Re-organise priorites of scripts

--- rootfs/etc/warpcode/install.sh
+++ rootfs/etc/warpcode/install.sh
@@ -5,7 +5,7 @@ set -e
 INSTALL_SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 cat "${INSTALL_SCRIPT_DIR}/banner.txt"
-. "${INSTALL_SCRIPT_DIR}/init/10-base-variables.sh"
+. "${INSTALL_SCRIPT_DIR}/init/00-base-variables.sh"
 for i in $(ls ${INSTALL_SCRIPT_DIR}/install/*.sh | sort -V)
 do
     . "$i"
