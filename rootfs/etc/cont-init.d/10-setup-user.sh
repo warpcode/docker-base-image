b9d2efb34dbec84ed6b72c4ca97e27f70be29304
Warpcode <git@warpcode.co.uk>
2021-02-02 21:53:30 +0000

    Update conditionals

--- rootfs/etc/cont-init.d/10-setup-user.sh
+++ rootfs/etc/cont-init.d/10-setup-user.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/with-contenv sh
 
-if [ "${PUID}" != "" ] && [ "${PUID}" != "$(id -u ${USERNAME})" ]; then
+if [ -n "${PUID}" ] && [ "${PUID}" != "$(id -u ${USERNAME})" ]; then
     usermod -o -u "${PUID}" "${USERNAME}" > /dev/null 2>&1
     s6-echo -- "[cont-init.d] Setting ${USERNAME} UID to ${PUID}"
 fi
 
-if [ "${PGID}" != "" ] && [ "${PGID}" != "$(id -g ${USERGROUP})" ]; then
+if [ -n "${PGID}" ] && [ "${PGID}" != "$(id -g ${USERGROUP})" ]; then
     groupmod -o -g "${PGID}" "${USERGROUP}" > /dev/null 2>&1
     s6-echo -- "[cont-init.d] Setting ${USERGROUP} GID to ${PGID}"
 fi
