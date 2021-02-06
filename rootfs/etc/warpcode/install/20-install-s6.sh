9fe5684a226d6d7a0996bc69e01f1f26eda66dd6
Warpcode <git@warpcode.co.uk>
2021-02-06 22:42:44 +0000

    Use url-fetch to install s6 overlay

--- rootfs/etc/warpcode/install/20-install-s6.sh
+++ rootfs/etc/warpcode/install/20-install-s6.sh
@@ -6,7 +6,7 @@ set -u
 OVERLAY_ARCH=$(arch | sed 's/x86_64/amd64/g' | sed 's/armv7l/armhf/g')
 
 if [ -z "$S6_VERSION" ] || [ "$S6_VERSION" = "latest" ]; then
-    S6_VERSION="$(get-url https://api.github.com/repos/just-containers/s6-overlay/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+    S6_VERSION="$(url-fetch "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | grep 'tag_name' | cut -d\" -f4)"
 
     if [ -z "$S6_VERSION" ]; then
         echo "Failed to grab the latest s6 version number"
@@ -18,7 +18,7 @@ S6_TAR_URL="https://github.com/just-containers/s6-overlay/releases/download/${S6
 
 echo "Downloading S6 Overlay ${S6_VERSION} for ${OVERLAY_ARCH}"
 echo "Downloading from $S6_TAR_URL"
-get-url "$S6_TAR_URL" > /tmp/s6-overlay.tar.gz
+url-fetch "$S6_TAR_URL" > /tmp/s6-overlay.tar.gz
 
 echo "Extracting S6 Overlay"
 tar xzf /tmp/s6-overlay.tar.gz -C / --exclude="./bin"
