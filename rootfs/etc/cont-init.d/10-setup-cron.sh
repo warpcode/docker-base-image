4e18870bd1574586710d857e8ca3478a30056ae0
Warpcode <git@warpcode.co.uk>
2021-02-11 21:55:34 +0000

    Remove useless cron check

--- rootfs/etc/cont-init.d/10-setup-cron.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv sh
-
-[ ! -d /etc/cron.d ] && mkdir -p /etc/cron.d
-
-if [ -n "$(ls -A /etc/cron.d 2>/dev/null)" ]
-then
-    # Only start cron if we have cron files
-    rm -f /etc/services.d/cron/down
-fi
