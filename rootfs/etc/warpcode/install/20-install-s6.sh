528de430fbdc726155e2ade880782e34367a18fb
Warpcode <git@warpcode.co.uk>
2021-05-30 21:34:24 +0100

    Use arguments rather than relying on custom env vars for url fetch

--- rootfs/etc/warpcode/install/20-install-s6.sh
+++ rootfs/etc/warpcode/install/20-install-s6.sh
@@ -5,8 +5,13 @@ set -u
 
 OVERLAY_ARCH=$(arch | sed 's/x86_64/amd64/g' | sed 's/armv7l/armhf/g')
 
+URL_FETCH_OPTIONS=""
+if [ "${IGNORE_CERTS:-0}" -eq "1" ]; then
+    URL_FETCH_OPTIONS="-n"
+fi
+
 if [ -z "$S6_VERSION" ] || [ "$S6_VERSION" = "latest" ]; then
-    S6_VERSION="$(url-fetch "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | grep 'tag_name' | cut -d\" -f4)"
+    S6_VERSION="$(url-fetch $URL_FETCH_OPTIONS "https://api.github.com/repos/just-containers/s6-overlay/releases/latest" | grep 'tag_name' | cut -d\" -f4)"
 
     if [ -z "$S6_VERSION" ]; then
         echo "Failed to grab the latest s6 version number"
@@ -18,7 +23,7 @@ S6_TAR_URL="https://github.com/just-containers/s6-overlay/releases/download/${S6
 
 echo "Downloading S6 Overlay ${S6_VERSION} for ${OVERLAY_ARCH}"
 echo "Downloading from $S6_TAR_URL"
-url-fetch "$S6_TAR_URL" > /tmp/s6-overlay.tar.gz
+url-fetch $URL_FETCH_OPTIONS "$S6_TAR_URL" > /tmp/s6-overlay.tar.gz
 
 echo "Extracting S6 Overlay"
 tar xzf /tmp/s6-overlay.tar.gz -C / --exclude="./bin"
